3119c265092cfcd2bce6ee900d2979c530aa52e8
IodiceAle <aleiodice2004@gmail.com>
2023-05-16 09:00:59 +0200

    z

--- kivy_venv/pyt.py
+++
@@ -1,4 +0,0 @@
-import json
-import requests
-
-requests.put("http://pigrinatorstand.local/newPath",data=json.dumps({"path":[{"direction": "forward","value":20},{"direction": "right","value":45}]}))
